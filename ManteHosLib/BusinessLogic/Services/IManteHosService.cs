a4298cff57f5e36c92359dd961fb38e61d7c922b
CUX "INDIO" PRADA <74674131+hoodrichpirobo@users.noreply.github.com>
2025-11-25 12:06:40 +0000

    Implement close work order service operations

--- ManteHosLib/BusinessLogic/Services/IManteHosService.cs
+++ ManteHosLib/BusinessLogic/Services/IManteHosService.cs
@@ -20,5 +20,21 @@ namespace ManteHos.Services
         // A partir de aquí los necesarios para los CU solicitados
         //
 
+        /// <summary>
+        /// Obtains the work orders assigned to the given operator that are still open.
+        /// </summary>
+        /// <param name="operatorId">Identifier of the operator whose work orders are requested.</param>
+        /// <returns>Enumerable containing the open work orders.</returns>
+        IEnumerable<WorkOrder> GetOpenWorkOrdersForOperator(string operatorId);
+
+        /// <summary>
+        /// Closes a work order providing the repair report and closing date.
+        /// </summary>
+        /// <param name="operatorId">Identifier of the operator performing the closure.</param>
+        /// <param name="workOrderId">Identifier of the work order to be closed.</param>
+        /// <param name="repairReport">Description of the work done.</param>
+        /// <param name="endDate">Date of closure. If null the current date will be used.</param>
+        void CloseWorkOrder(string operatorId, int workOrderId, string repairReport, DateTime? endDate = null);
+
     }
 }
